674c7524f50333dee96464e382838d62af7b3659
HarryBaek <01697550772@naver.com>
2021-11-17 15:47:27 +0900

    test

---
+++ flowchart_01.py
@@ -0,0 +1,19 @@
+git config --global user.email "01697550772@naver.com"
+git config --global user.name "HarryBaek"
+
+git config --list
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+if b == 2:
+    a == 10
+
+else :
+    c = 20
+
+c = 10 + d
+
+print('')
